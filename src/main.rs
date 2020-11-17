114729b890291f2171e9a414ce103ddf9de2c9fb
David Weis <dweis7@gmail.com>
2020-11-17 03:54:55 +0000

    Add ping endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-fn main() {
-    println!("Hello, world! {}", do_thing());
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
 }
 
-fn do_thing() -> bool {
-    true
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_method() {
-        assert!(do_thing());
-    }
 }
