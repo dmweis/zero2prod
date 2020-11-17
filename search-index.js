99c196f3d1863447c4e811fbda9ffca713e7aae5
dmweis <dmweis@users.noreply.github.com>
2020-11-17 05:43:25 +0000

    deploy: 0310f0578e289141b648894a5420b6def8003f57

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
 "app":{"doc":"","i":[[5,"main","app","",null,[[],["result",6]]]],"p":[]},\
-"zero2prod":{"doc":"","i":[[5,"health_check","zero2prod","",null,[[]]],[5,"run","","",null,[[]]]],"p":[]}\
+"zero2prod":{"doc":"","i":[[5,"health_check","zero2prod","",null,[[]]],[5,"run","","",null,[[],[["error",3],["server",3],["result",4]]]]],"p":[]}\
 }');
 addSearchOptions(searchIndex);initSearch(searchIndex);
