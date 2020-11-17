5196b9b0f621d86325b504eb1294e1f5851c0bfc
David Weis <dweis7@gmail.com>
2020-11-17 03:09:54 +0000

    Add method and passing test to test ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn do_thing() -> bool {
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_method() {
+        assert!(do_thing());
+    }
+}
