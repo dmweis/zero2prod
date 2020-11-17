ef61b7bebc0e9ee09ce98397e87c73a279133e42
dmweis <dmweis@users.noreply.github.com>
2020-11-17 03:39:11 +0000

    deploy: 1c22cb8ea5211174e2754efd8be5b7f1d6678ac7

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"zero2prod":{"doc":"","i":[[5,"main","zero2prod","",null,[[]]],[5,"do_thing","","",null,[[]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["zero2prod"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
