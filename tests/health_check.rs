f449605f76262be69dab292ae77a82e0e9996ba4
David Weis <dweis7@gmail.com>
2020-11-20 09:41:10 +0000

    Fix incorrect header type

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -120,7 +120,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         // Act
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-ww-form-urlencoded")
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
             .await
