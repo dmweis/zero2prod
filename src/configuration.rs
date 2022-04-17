50f18cbfb16745efdcae8f8687b9957a5e94cf4f
David Weis <davidmichaelweis@gmail.com>
2022-04-17 04:43:54 +0100

    Add config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,19 @@
-
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+    // TODO(David): Upgrade to new version with config builder
+    settings.merge(config::File::with_name("configuration"))?;
+    settings.try_into()
+}
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind random port");
-
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("0.0.0.0:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
 }
