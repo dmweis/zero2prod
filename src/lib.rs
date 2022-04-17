5bb432be50b5a36208ee0f39cabecf6e943248ea
David Weis <davidmichaelweis@gmail.com>
2022-04-17 04:37:48 +0100

    Restructure and add config

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,5 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    println!(
-        "New subscriber named {} with email {}",
-        form.name, form.email
-    );
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub use startup::run;
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,15 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    println!(
+        "New subscriber named {} with email {}",
+        form.name, form.email
+    );
+    HttpResponse::Ok().finish()
+}
