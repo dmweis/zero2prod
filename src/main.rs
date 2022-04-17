0dfb1826a51dfced0a3c25ecf9e9bbce7739e169
David Weis <davidmichaelweis@gmail.com>
2022-04-17 02:12:03 +0100

    Add some deps and test to test docker image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use log::*;
+use simplelog::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    CombinedLogger::init(vec![TermLogger::new(
+        LevelFilter::Info,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )])
+    .unwrap();
+
+    info!("Hello, world!");
+    info!("Or does it?");
+    info!("I guess it does!");
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    // use super::*;
+
+    #[test]
+    fn passing_test() {
+        assert_eq!(true, true);
+    }
 }
