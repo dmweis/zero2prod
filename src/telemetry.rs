3c94bf56f6c16a22b7f0f5b875d837efd9948df3
David Weis <davidmichaelweis@gmail.com>
2022-04-18 01:50:46 +0100

    Tune verbosity

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -40,7 +40,7 @@ where
     } else {
         let compact = fmt::layer()
             .with_writer(sink)
-            .with_span_events(FmtSpan::ACTIVE)
+            .with_span_events(FmtSpan::ENTER)
             .pretty();
         layers.push(compact.boxed());
     }
