b224d8b0cc110ba07308e1d00909b0101fd0f2e5
David Weis <davidmichaelweis@gmail.com>
2022-04-18 04:16:10 +0100

    Make timeout configuratble

--- src/email_client.rs
+++ src/email_client.rs
@@ -14,11 +14,9 @@ impl EmailClient {
         base_url: String,
         sender: SubscriberEmail,
         authorization_token: Secret<String>,
+        timeout: std::time::Duration,
     ) -> Self {
-        let http_client = Client::builder()
-            .timeout(std::time::Duration::from_secs(10))
-            .build()
-            .unwrap();
+        let http_client = Client::builder().timeout(timeout).build().unwrap();
         Self {
             http_client,
             base_url,
@@ -115,7 +113,12 @@ mod tests {
     }
     /// Get a test instance of `EmailClient`.
     fn email_client(base_url: String) -> EmailClient {
-        EmailClient::new(base_url, email(), Secret::new(Faker.fake()))
+        EmailClient::new(
+            base_url,
+            email(),
+            Secret::new(Faker.fake()),
+            std::time::Duration::from_millis(200),
+        )
     }
 
     #[tokio::test]
@@ -179,8 +182,6 @@ mod tests {
 
     #[tokio::test]
     async fn send_email_times_out_if_the_server_takes_too_long() {
-        // TODO(David): I hate timeout tests :/
-
         // Arrange
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
