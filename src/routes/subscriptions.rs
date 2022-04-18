ec9bf935160970214327ef929d2c4f688791c206
David Weis <davidmichaelweis@gmail.com>
2022-04-18 02:53:25 +0100

    Add full name and email validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -22,12 +22,19 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let name = match SubscriberName::parse(form.0.name) {
         Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
+        Err(e) => {
+            tracing::warn!(error = %e, "Encountered invalid name");
+            return HttpResponse::BadRequest().finish();
+        }
     };
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(e) => {
+            tracing::warn!(error = %e, "Encountered invalid email");
+            return HttpResponse::BadRequest().finish();
+        }
     };
+    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -48,7 +55,7 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
     "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
